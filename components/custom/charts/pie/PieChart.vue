59f9c3236b0eb58579ef9e83221d9d1b2a9681e0
LucasMatusalem <lucas@matusa.dev>
2024-11-01 11:35:53 -0300

    initial commit

---
+++ components/custom/charts/pie/PieChart.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { DonutChart } from '@/components/ui/chart-donut'
+
+const data = [
+  {
+    name: 'critical',
+    total: 3000,
+  },
+  {
+    name: 'high',
+    total: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: 'medium',
+    total: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: 'low',
+    total: Math.floor(Math.random() * 2000) + 500,
+  },
+  {
+    name: 'unknown',
+    total: Math.floor(Math.random() * 2000) + 500,
+  },
+]
+</script>
+
+<template>
+  <DonutChart index="name" :category="'total'" :data="data" :type="'donut'" >
+  </DonutChart>
+</template>
---
+++ pages/dashboard.vue
@@ -0,0 +1,142 @@
+<script setup lang="ts">
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import MainNav from '@/components/custom/MainNav.vue'
+
+import OvertimeChart from '@/components/custom/charts/overtime/OvertimeChart.vue'
+import PieChart from '@/components/custom/charts/pie/PieChart.vue'
+
+
+import UserNav from '@/components/custom/UserNav.vue'
+
+</script>
+
+<template>
+  <div class="md:hidden">
+    <VPImage alt="Dashboard" width="1280" height="1214" class="block" :image="{
+      dark: '/examples/dashboard-dark.png',
+      light: '/examples/dashboard-light.png',
+    }" />
+  </div>
+
+  <div class="hidden flex-col md:flex">
+    <div class="border-b">
+      <div class="flex h-16 items-center px-4">
+        <!-- <TeamSwitcher /> -->
+        <MainNav class="mx-6" />
+        <div class="ml-auto flex items-center space-x-4">
+          <UserNav />
+        </div>
+      </div>
+    </div>
+    <div class="flex-1 space-y-4 p-8 pt-6">
+      <div class="flex items-center justify-between space-y-2">
+        <h2 class="text-3xl font-bold tracking-tight">
+          Dashboard
+        </h2>
+      </div>
+      <div class="pb-4">
+        <div class="grid gap-4 md:grid-cols-1 lg:grid-cols-3">
+          <Card>
+            <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle class="text-sm font-medium">
+                Total Revenue
+              </CardTitle>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="h-4 w-4 text-muted-foreground">
+                <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+              </svg>
+            </CardHeader>
+            <CardContent>
+              <div class="text-2xl font-bold">
+                $45,231.89
+              </div>
+              <p class="text-xs text-muted-foreground">
+                +20.1% from last month
+              </p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle class="text-sm font-medium">
+                Subscriptions
+              </CardTitle>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="h-4 w-4 text-muted-foreground">
+                <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                <circle cx="9" cy="7" r="4" />
+                <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+              </svg>
+            </CardHeader>
+            <CardContent>
+              <div class="text-2xl font-bold">
+                +2350
+              </div>
+              <p class="text-xs text-muted-foreground">
+                +180.1% from last month
+              </p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle class="text-sm font-medium">
+                Sales
+              </CardTitle>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="h-4 w-4 text-muted-foreground">
+                <rect width="20" height="14" x="2" y="5" rx="2" />
+                <path d="M2 10h20" />
+              </svg>
+            </CardHeader>
+            <CardContent>
+              <div class="text-2xl font-bold">
+                +12,234
+              </div>
+              <p class="text-xs text-muted-foreground">
+                +19% from last month
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+      <div class="grid gap-4 md:grid-cols-1 lg:grid-cols-3">
+        <Card class="col-span-2">
+          <CardHeader>
+            <CardTitle>Overtime</CardTitle>
+          </CardHeader>
+          <CardContent class="pl-0">
+            <OvertimeChart />
+          </CardContent>
+        </Card>
+        <Card class="col-span-1">
+          <CardHeader>
+            <CardTitle>Overtime</CardTitle>
+          </CardHeader>
+          <CardContent class="pl-0">
+            <PieChart></PieChart>
+          </CardContent>
+        </Card>
+        <Card class="col-span-1">
+          <CardHeader>
+            <CardTitle>Overtime</CardTitle>
+          </CardHeader>
+          <CardContent class="pl-0">
+            <PieChart></PieChart>
+          </CardContent>
+        </Card>
+        <Card class="col-span-2">
+          <CardHeader>
+            <CardTitle>Overtime</CardTitle>
+          </CardHeader>
+          <CardContent class="pl-0">
+            <OvertimeChart />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  </div>
+</template>
